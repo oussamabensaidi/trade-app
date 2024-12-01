0db0405c198c4d8f7a8eef2f9c79623bc5ef9a9c
oussama <bensaidioussama7@gmail.com>
2024-12-01 16:20:07 +0100

    version intial

--- resources/views/todolist/index.blade.php
+++ resources/views/todolist/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>toDoList</title>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.7/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
